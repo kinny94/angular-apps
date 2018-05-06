f053ea1c2b7afb9bf20143ae61978b7ecbe10c19
kinny94 <ad.sam007@gmail.com>
2018-05-06 18:36:10 -0400

    Working with ngIf directive

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,6 +10,7 @@ export class ServersComponent implements OnInit {
 	allowNewServer: boolean = false;
 	serverCreationStatus: string = 'No server was created!!';
 	serverName: string = 'test';
+	serverCreated: boolean = false;
 
 	constructor() { 
 		setTimeout(() => {
@@ -25,6 +26,7 @@ export class ServersComponent implements OnInit {
 	}
 
 	onCreateServer = () => {
-		this.serverCreationStatus = 'Server was created!!';
+		this.serverCreated = true;
+		this.serverCreationStatus = 'Server was created!! Name is ' + this.serverName; 
 	}
 }
