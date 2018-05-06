125695e80cb84ec21c64d8de6b0da5a8565b4870
kinny94 <ad.sam007@gmail.com>
2018-05-06 16:20:38 -0400

    Added antoher component using cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,20 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 
 @NgModule({
 	declarations: [
 		AppComponent,
-		ServerComponent
+		ServerComponent,
+		ServersComponent
 	],
 	imports: [
 		BrowserModule,
 		FormsModule
 	],
 	providers: [],
-	bootstrap: [AppComponent, ServerComponent]
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
