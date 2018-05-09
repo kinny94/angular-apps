66b29d5b87ce18cb2aef83d8eb0252e05306f25d
kinny94 <ad.sam007@gmail.com>
2018-05-08 23:32:17 -0400

    Another way of referencing

--- Playground/Lifecycle Explanation/src/app/cockpit/cockpit.component.ts
+++ Playground/Lifecycle Explanation/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewChild } from '@angular/core';
 
 @Component({
 	selector: 'app-cockpit',
@@ -10,21 +10,21 @@ export class CockpitComponent implements OnInit {
 	@Output() serverCreated = new EventEmitter<{ serverName:string, serverContent: string }>();
 	@Output() blueprintCreated = new EventEmitter<{ serverName:string, serverContent: string }>();;
 
-	newServerName = '';
-	newServerContent = '';
-	
+	// newServerName = '';
+	// newServerContent = '';
+	@ViewChild('serverContentInput') serverContentInput
+
 	constructor() { }
 	
 	ngOnInit() {
 	}
 
 	onAddServer( nameInput: HTMLInputElement ){
-		console.log( nameInput.value );
-		this.serverCreated.emit({ serverName: nameInput.value, serverContent: this.newServerContent });
+		this.serverCreated.emit({ serverName: nameInput.value, serverContent: this.serverContentInput.nativeElement.value });
 	}
 
-	onAddBlueprint(){
-		this.blueprintCreated.emit({ serverName: this.newServerName, serverContent: this.newServerContent });
+	onAddBlueprint( nameInput: HTMLInputElement ){
+		this.blueprintCreated.emit({ serverName: nameInput.value, serverContent: this.serverContentInput.nativeElement.value });
 	}
 	
 }
