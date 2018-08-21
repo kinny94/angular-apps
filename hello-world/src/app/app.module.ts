cca5daf4ed1a1120e2fed6733b1d8f826c6d036d
kinny94 <adass@onbase.net>
2018-08-21 12:05:16 -0400

    added my first component

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -2,15 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+
+		//including our components
+		AppComponent,
+		CoursesComponent
+	],
+	imports: [
+		BrowserModule
+	],
+	providers: [],
+	bootstrap: [AppComponent ]
 })
 export class AppModule { }
---
+++ hello-world/src/app/courses.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    // properties to tell angular how this component works
+    selector: 'courses', // <div id="courses">
+    template: '<h4>Courses</h4>'    //htlml amrkup to be rendered for this component 
+})
+
+export class CoursesComponent{
+
+}
