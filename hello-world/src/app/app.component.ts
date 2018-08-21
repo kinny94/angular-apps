0f80bf74c3ea97804fe8acf0352876e817a1915f
kinny94 <adass@onbase.net>
2018-08-21 16:16:54 -0400

    worked on changing the dom

--- hello-world/src/app/app.component.ts
+++ hello-world/src/app/app.component.ts
@@ -8,6 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 	title = 'Angular App';
-	courses = [ 1, 2  ]; 
-	viewMode = 'somethinf';
+	viewMode = 'something else';
+	courses = [
+		{
+			id: 1, name: 'course 1'
+		},
+		{
+			id: 2, name: 'course 2'
+		},{
+			id: 3, name: 'course 3'
+		}
+	] 
+	
+	onAdd(){
+		this.courses.push({
+			id: 4,
+			name: 'course4'
+		});
+	}
+
+	onRemove( course ){
+		let index = this.courses.indexOf( course );
+		this.courses.splice( index, 1 );
+	}
+
+	onChange( course ){
+		course.name = "Updated"
+	}
+
 }
