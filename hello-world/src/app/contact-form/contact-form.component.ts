d1eebe1625f286eab96c094352103b67e1c37255
kinny94 <adass@onbase.net>
2018-08-22 11:40:36 -0400

    Worked in angular forms drop down , check boxes

--- hello-world/src/app/contact-form/contact-form.component.ts
+++ hello-world/src/app/contact-form/contact-form.component.ts
@@ -9,8 +9,18 @@ export class ContactFormComponent{
 	
 	constructor() { }
 	
+	contactMethods = [
+		{ id:1, name: 'Email' },
+		{ id:2, name: 'Telephone' },
+		{ id:3, name: 'Fax' },
+		{ id:4, name: 'Text' }
+	]
 	log( x ){
 		console.log( x );
+	}
+	 
+	submit( f ){
+		console.log( f );
 	}
 	
 }
