7c1b7453f3eaea87b07981404aed968aa31e827f
kinny94 <adass@onbase.net>
2018-08-23 10:15:06 -0400

    implemented update, post delete request

--- hello-world/src/app/post-component/post-component.component.ts
+++ hello-world/src/app/post-component/post-component.component.ts
@@ -9,14 +9,44 @@ import { Http } from '@angular/http';
 export class PostComponentComponent implements OnInit {
 	
 	posts: any[];
+	private url = 'https://jsonplaceholder.typicode.com/posts';
 
-	constructor( http: Http ){ 
-		http.get( 'https://jsonplaceholder.typicode.com/posts' )
+	constructor( private http: Http ){ 
+		http.get( this.url )
 		.subscribe( response =>  {
 			this.posts = response.json();
 		})
 	}
 	
+	createPost( input: HTMLInputElement ){
+		let post = { title: input.value }
+		input.value = '';
+
+		this.http.post( this.url, JSON.stringify( post ))
+		.subscribe( response => {
+			post['id'] =  response.json().id;
+			this.posts.splice( 0, 0, post );
+		})
+	}
+
+	updatePost( post ){
+		this.http.patch( this.url + '/' + post.id, JSON.stringify({
+			isRead: true 
+		})).subscribe( response => {
+			console.log( response.json() );
+		})
+		//	this.http.put( this.url, JSON.stringify( post ));
+	} 
+
+	deletePost( post ){
+		this.http.delete( this.url + '/' + post.id )
+		.subscribe( response => {
+			let index = this.posts.indexOf( post );
+			this.posts.splice( index, 1 );
+		});
+	}
+
+
 	ngOnInit() {
 	}
 	
