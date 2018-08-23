4e00a0198ee7d6ede7aae7006b0cf095e6813cbe
kinny94 <adass@onbase.net>
2018-08-23 15:46:25 -0400

    Working on routing

--- route-app/src/app/app.module.ts
+++ route-app/src/app/app.module.ts
@@ -1,16 +1,51 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { ProfileComponentComponent } from './profile-component/profile-component.component';
+import { FollowersComponent } from './followers/followers.component';
+import { NavbarComponentComponent } from './navbar-component/navbar-component.component';
+import { PostsComponentComponent } from './posts-component/posts-component.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HomeComponentComponent,
+		ProfileComponentComponent,
+		FollowersComponent,
+		NavbarComponentComponent,
+		PostsComponentComponent,
+		NotFoundComponent
+	],
+	imports: [
+		BrowserModule,
+		RouterModule.forRoot([
+			{
+				path: '',
+				component: HomeComponentComponent
+			},
+			{
+				path: 'followers',
+				component: FollowersComponent
+			},
+			{
+				path: 'profile/:username',
+				component: ProfileComponentComponent	
+			},
+			{
+				path: 'posts',
+				component: PostsComponentComponent
+			},
+			{
+				path: '**',
+				component: NotFoundComponent	
+			}
+		])
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
+
 export class AppModule { }
