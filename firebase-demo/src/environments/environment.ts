5e05620e7a3ff203e42de6eedf026332bd0b2f72
kinny94 <ad.sam007@gmail.com>
2018-08-24 13:18:19 -0400

    set up firebase in my app

--- firebase-demo/src/environments/environment.ts
+++ firebase-demo/src/environments/environment.ts
@@ -3,13 +3,21 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+	production: false,
+	firebase:{
+		apiKey: "AIzaSyAqgJZnSiRmxyDidVwBkYmUXOSz3K56N18",
+		authDomain: "fs1prod-4323d.firebaseapp.com",
+		databaseURL: "https://fs1prod-4323d.firebaseio.com",
+		projectId: "fs1prod-4323d",
+		storageBucket: "fs1prod-4323d.appspot.com",
+		messagingSenderId: "32477203838"
+	}
 };
 
 /*
- * In development mode, for easier debugging, you can ignore zone related error
- * stack frames such as `zone.run`/`zoneDelegate.invokeTask` by importing the
- * below file. Don't forget to comment it out in production mode
- * because it will have a performance impact when errors are thrown
- */
+* In development mode, for easier debugging, you can ignore zone related error
+* stack frames such as `zone.run`/`zoneDelegate.invokeTask` by importing the
+* below file. Don't forget to comment it out in production mode
+* because it will have a performance impact when errors are thrown
+*/
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
