4347086888e52facffa214226b6b6070ad5c2bdc
kinny94 <ad.sam007@gmail.com>
2018-08-24 15:09:35 -0400

    Wokring on angular material

--- angular-material/src/app/app.module.ts
+++ angular-material/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		BrowserAnimationsModule,
+		MatCheckboxModule
+
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
+ 
