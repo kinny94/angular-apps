6159ce2a00b57efd99654f2de4b394f9bea67dd0
kinny94 <ad.sam007@gmail.com>
2018-08-24 14:01:04 -0400

    not working firebase demo

--- firebase-demo/src/app/app.component.ts
+++ firebase-demo/src/app/app.component.ts
@@ -1,10 +1,18 @@
+import { Observable } from 'rxjs';
 import { Component } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'firebase-demo';
+
+	courses: Observable<any[]>;
+	
+	constructor( db: AngularFireDatabase ){
+		let items = db.list( 'fs1prod-4323d' );
+		
+	}
 }
