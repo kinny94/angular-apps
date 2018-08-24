6ea4c6323f472cc9c58eea7c925944120658cca4
kinny94 <ad.sam007@gmail.com>
2018-08-24 11:18:59 -0400

    set up routes and services

--- authentication-app/src/app/app.module.ts
+++ authentication-app/src/app/app.module.ts
@@ -1,3 +1,8 @@
+import { AuthService } from './services/auth.service';
+import { OrderService } from './services/order.service';
+import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -8,17 +13,40 @@ import { LoginComponentComponent } from './login-component/login-component.compo
 import { NoAccessComponentComponent } from './no-access-component/no-access-component.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponentComponent,
-    AdminComponentComponent,
-    LoginComponentComponent,
-    NoAccessComponentComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HomeComponentComponent,
+		AdminComponentComponent,
+		LoginComponentComponent,
+		NoAccessComponentComponent
+	],
+	imports: [
+		BrowserModule,
+		FormsModule,
+		HttpModule,
+		RouterModule.forRoot([
+			{
+				path: '',
+				component: HomeComponentComponent
+			},
+			{
+				path: 'admin',
+				component: AdminComponentComponent
+			},
+			{
+				path: 'login',
+				component: LoginComponentComponent
+			},
+			{
+				path: 'no-access',
+				component: NoAccessComponentComponent
+			}
+		])
+	],
+	providers: [
+		OrderService,
+		AuthService
+	],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
