1055342a29dfa4ac45f779652eca9eb480ec372e
Arjun Dass <ad.sam007@gmail.com>
2019-08-20 12:08:55 -0400

    ViewChild decorator

--- angular-core/src/app/app.component.ts
+++ angular-core/src/app/app.component.ts
@@ -1,5 +1,6 @@
+import { CourseCardComponent } from './course-card/course-card.component';
 import { Course } from './model/course';
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import {COURSES} from '../db-data';
 
 @Component({
@@ -9,9 +10,12 @@ import {COURSES} from '../db-data';
 })
 export class AppComponent {
 
+  @ViewChild(CourseCardComponent, { read: '', static: false })
+  card: CourseCardComponent;
+
   courses = COURSES;
 
   onCourseSelected(course: Course) {
-    console.log(course);
+    console.log(this.card);
   }
 }
